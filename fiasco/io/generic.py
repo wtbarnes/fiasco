65f652b6d313d764a73bad063efec726636af772
Will Barnes <wtb2@rice.edu>
2017-08-12 01:42:31 -0500

    generalize base class for non-ion files

--- fiasco/io/generic.py
+++ fiasco/io/generic.py
@@ -1,10 +1,8 @@
 """
 Base class for file parser
 """
-
 import os
 from astropy.table import QTable
-
 import fiasco
 
 
@@ -17,25 +15,25 @@ class GenericParser(object):
     units = []
     headings = []
     
-    def __init__(self,ion_filename):
+    def __init__(self,ion_filename,**kwargs):
         self.ion_filename = ion_filename
         self.filetype = self.ion_filename.split('.')[-1]
         self.ion_name = self.ion_filename.split('.')[0]
         self.element = self.ion_name.split('_')[0]
+        self.full_path = os.path.join(fiasco.defaults['chianti_dbase_root'],
+                                      self.element,self.ion_name,self.ion_filename)
         
     def parse(self):
         """
         Generate Astropy QTable from a CHIANTI ion file 
         """
-        full_path = os.path.join(fiasco.defaults['chianti_dbase_root'],self.element,
-                                 self.ion_name,self.ion_filename)
-        if not os.path.isfile(full_path):
+        if not os.path.isfile(self.full_path):
             return None
-        with open(full_path,'r') as f:
+        with open(self.full_path, 'r') as f:
             lines = f.readlines()
         table = []
         for i,line in enumerate(lines):
-            line = list(filter(None,line.strip().split('  ')))
+            line = list(filter(None, line.strip().split('  ')))
             if line[0] == '-1':
                 comment = ''.join(lines[i+1:len(lines)])
                 break
@@ -43,13 +41,12 @@ class GenericParser(object):
                 self.preprocessor(table,line,i)
 
         df = QTable(data=list(map(list,zip(*table))), names=self.headings)
-        for name,unit,dtype in zip(self.headings,self.units,self.dtypes):
+        for name,unit,dtype in zip(self.headings, self.units, self.dtypes):
             df[name].unit = unit
             df[name] = df[name].astype(dtype)
         
         df.meta['footer'] = comment
-        df.meta['element'] = self.element
-        df.meta['ion'] = self.ion_name
+
         df = self.postprocessor(df)
         
         return df
@@ -64,6 +61,8 @@ class GenericParser(object):
         """
         Default postprocessor method run on the whole dataframe
         """
+        df.meta['element'] = self.element
+        df.meta['ion'] = self.ion_name
         return df
     
     def to_hdf5(self,hf):
