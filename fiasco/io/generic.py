5ea8445f04804916789cf9c547260fddf3330935
Will Barnes <wtb2@rice.edu>
2017-08-13 14:54:10 -0500

    append chianti version to all tables

--- fiasco/io/generic.py
+++ fiasco/io/generic.py
@@ -63,6 +63,10 @@ class GenericParser(object):
         """
         df.meta['element'] = self.element
         df.meta['ion'] = self.ion_name
+        with open(os.path.join(fiasco.defaults['chianti_dbase_root'],'VERSION'),'r') as f:
+            lines = f.readlines()
+            version = lines[0].strip()
+        df.meta['chianti_version'] = version
         return df
     
     def to_hdf5(self,hf):
