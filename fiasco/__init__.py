285d8cd5442948c357110b7bac18f82bf0b89aeb
Will Barnes <wtb2@rice.edu>
2017-10-24 00:45:20 -0500

    Base element class for organizing groups of ions; TODO: will need an element for the Ion class (another TODO), do we need two?

--- fiasco/__init__.py
+++ fiasco/__init__.py
@@ -4,7 +4,7 @@ fiasco
 
 a prototype package for interfacing with the CHIANTI atomic database
 """
-from .base import IonBase
+from .base import IonBase, ElementBase
 
 from .util import setup_paths
 defaults = setup_paths()
--- fiasco/base.py
+++ fiasco/base.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import h5py
 import astropy.units as u
+import periodictable
 
 import fiasco
 from .io.factory import all_subclasses
@@ -18,6 +19,12 @@ class DataIndexer(object):
     def __init__(self, hdf5_path, top_level_path):
         self.top_level_path = top_level_path
         self.hdf5_dbase_root = hdf5_path
+
+    @property
+    def fields(self):
+        with h5py.File(self.hdf5_dbase_root, 'r') as hf:
+            fields = [k for k in hf[self.top_level_path]]
+        return fields
     
     def __contains__(self, key):
         with h5py.File(self.hdf5_dbase_root, 'r') as hf:
@@ -73,7 +80,7 @@ class IonBase(object):
     def __init__(self, ion_name, hdf5_path=None, **kwargs):
         self.ion_name = ion_name
         self.element = ion_name.split('_')[0]
-        self.stage = ion_name.split('_')[-1]
+        self.stage = int(ion_name.split('_')[-1])
         if hdf5_path is None:
             self.hdf5_dbase_root = fiasco.defaults['hdf5_dbase_root']
         else:
@@ -106,3 +113,47 @@ all_ext = [cls.filetype for cls in all_subclasses(GenericParser)
 for filetype in all_ext:
     add_property(IonBase, filetype)
     add_property(IonBase, '/'.join(['dielectronic', filetype]))
+
+
+class ElementBase(object):
+    """
+    Base class for accessing CHIANTI data attached to a particular element
+    """
+
+    def __init__(self, element_name, hdf5_path=None, **kwargs):
+        if type(element_name) is str:
+            self.element_name = element_name.lower()
+        elif type(element_name) is int:
+            self.element_name = periodictable.elements[element_name].symbol.lower()
+        else:
+            raise ValueError('Unrecognized input type {}'.format(type(element_name)))
+        if hdf5_path is None:
+            self.hdf5_dbase_root = fiasco.defaults['hdf5_dbase_root']
+        else:
+            self.hdf5_dbase_root = hdf5_path
+
+    @property
+    def ions(self):
+        with h5py.File(self.hdf5_dbase_root, 'r') as hf:
+            ions = sorted([i.split('_') for i in hf[self.element_name].keys() 
+                           if '{}_'.format(self.element_name) in i],
+                          key=lambda x: int(x[1]))
+        return ['_'.join(i) for i in ions]
+
+    def __getitem__(self, x):
+        if type(x) is int:
+            x = self.ions[x]
+        return IonBase(x, hdf5_path=self.hdf5_dbase_root)
+
+    def __contains__(self, x):
+        return x in self.ions
+
+    def __repr__(self):
+        ion_list = ['{} {}'.format(i.split('_')[0].capitalize(), i.split('_')[1]) for i in self.ions]
+        return '''Element
+-------
+{}
+
+Available Ions
+--------------
+{}'''.format(self.element_name.capitalize(), '\n'.join(ion_list))
