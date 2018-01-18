8a2697ba6ae861194e214395b442d04d8dc46ee6
Will Barnes <wtb2@rice.edu>
2018-01-18 15:13:47 -0600

    More flexibility in factory to allow for parser creation from name; use factory with standalone files

--- fiasco/io/factory.py
+++ fiasco/io/factory.py
@@ -1,6 +1,7 @@
 """
 Factory and interface for file parser
 """
+import os
 import warnings
 
 from .sources import *
@@ -13,6 +14,9 @@ class ParserFactory(type):
     """
 
     def __call__(cls, *args, **kwargs):
+        # Allow for standalone files
+        if os.path.exists(args[0]):
+            kwargs['full_path'] = args[0]
         # Use custom parser if desired
         custom_parser = None
         if 'custom_parser' in kwargs:
@@ -20,13 +24,16 @@ class ParserFactory(type):
             del kwargs['custom_parser']
         if custom_parser is not None:   
             return custom_parser(*args, **kwargs)
-        # Create parser based on file extension
-        filetype = args[0].split('.')[-1]
+        # Create parser based on file extension or name
+        filetype_name, filetype_ext = os.path.splitext(os.path.basename(args[0]))
+        filetype_ext = filetype_ext[1:]
         subclass_dict = {c.filetype: c for c in all_subclasses(GenericParser) if hasattr(c, 'filetype')}
-        if filetype in subclass_dict:
-            return subclass_dict[filetype](*args, **kwargs)
+        if filetype_ext in subclass_dict:
+            return subclass_dict[filetype_ext](*args, **kwargs)
+        elif filetype_name in subclass_dict:
+            return subclass_dict[filetype_name](*args, **kwargs)
         else:
-            warnings.warn('Unrecognized file extension {}'.format(filetype), stacklevel=2)
+            warnings.warn('Unrecognized filename and extension {}'.format(args[0]), stacklevel=2)
             return type.__call__(cls, *args, **kwargs)
 
 
