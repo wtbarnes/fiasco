8d9693c1c3fe6b0e14453297a8ffcf65b961ecd9
Will Barnes <wtb2@rice.edu>
2018-04-05 12:13:31 -0500

    use functools to allow for parsing of func signatures

--- fiasco/util/decorators.py
+++ fiasco/util/decorators.py
@@ -1,6 +1,8 @@
 """
 Useful function/method decorators
 """
+from functools import wraps
+
 __all__ = ['needs_dataset']
 
 
@@ -12,6 +14,7 @@ def needs_dataset(*names, default=None):
     names = [f'_{n}' if n not in non_ion_datasets else f'{n}' for n in names]
 
     def decorator(func):
+        @wraps(func)
         def func_wrapper(*args, **kwargs):
             if any([args[0].__getattribute__(n) is None for n in names]):
                 return default
