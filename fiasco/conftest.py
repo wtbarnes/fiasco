f944b8195b6c0168632059b48e09da2b161233cb
Will Barnes <wtb2@rice.edu>
2020-03-02 00:56:46 -0500

    use fixtures to build a minimal version of the database

--- fiasco/conftest.py
+++ fiasco/conftest.py
@@ -1,46 +1,45 @@
 # this contains imports plugins that configure py.test for astropy tests.
 # by importing them here in conftest.py they are discoverable by py.test
 # no matter how it is invoked within the source tree.
-
+import pytest
 from astropy.tests.plugins import *
 
-## Uncomment the following line to treat all DeprecationWarnings as
-## exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
-## as follow (although default should work for most cases).
-## To ignore some packages that produce deprecation warnings on import
-## (in addition to 'compiler', 'scipy', 'pygments', 'ipykernel', and
-## 'setuptools'), add:
-##     modules_to_ignore_on_import=['module_1', 'module_2']
-## To ignore some specific deprecation warning messages for Python version
-## MAJOR.MINOR or later, add:
-##     warnings_to_ignore_by_pyver={(MAJOR, MINOR): ['Message to ignore']}
-# enable_deprecations_as_exceptions()
+from fiasco.util import build_hdf5_dbase, download_dbase
+from fiasco.util.setup_db import CHIANTI_URL, LATEST_VERSION
 
-## Uncomment and customize the following lines to add/remove entries from
-## the list of packages for which version numbers are displayed when running
-## the tests. Making it pass for KeyError is essential in some cases when
-## the package uses other astropy affiliated packages.
-# try:
-#     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
-#     PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
-#     del PYTEST_HEADER_MODULES['h5py']
-# except (NameError, KeyError):  # NameError is needed to support Astropy < 1.0
-#     pass
+# Minimal set of CHIANTI files needed to run the tests
+TEST_FILES = [
+    'sun_photospheric_1998_grevesse.abund',
+    'chianti.ip',
+    'chianti.ioneq',
+    'h_1.elvlc',
+    'h_2.rrparams',
+    'he_1.elvlc',
+    'he_2.elvlc',
+    'he_3.rrparams',
+    'li_1.diparams',
+    'ca_2.elvlc',
+    'fe_2.elvlc',
+    'fe_5.elvlc',
+    'fe_6.elvlc',
+    'fe_9.elvlc',
+    'fe_12.elvlc',
+    'fe_15.elvlc',
+    'fe_18.rrparams',
+    'fe_18.drparams',
+    'fe_27.rrparams',
+]
 
-## Uncomment the following lines to display the version number of the
-## package rather than the version number of Astropy in the top line when
-## running the tests.
-# import os
-#
-## This is to figure out the package version, rather than
-## using Astropy's
-# try:
-#     from .version import version
-# except ImportError:
-#     version = 'dev'
-#
-# try:
-#     packagename = os.path.basename(os.path.dirname(__file__))
-#     TESTED_VERSIONS[packagename] = version
-# except NameError:   # Needed to support Astropy <= 1.0.0
-#     pass
+
+@pytest.fixture(scope='session')
+def ascii_dbase_root(tmpdir_factory):
+    path = tmpdir_factory.mktemp('chianti_dbase')
+    download_dbase(CHIANTI_URL.format(version=LATEST_VERSION), path)
+    return path
+
+
+@pytest.fixture(scope='session')
+def hdf5_dbase_root(ascii_dbase_root, tmpdir_factory):
+    path = tmpdir_factory.mktemp('fiasco').join('chianti_dbase.h5')
+    build_hdf5_dbase(ascii_dbase_root, path, files=TEST_FILES)
+    return path
