bac1e1cd2e637e865eca876516330e7cc391bf67
Will Barnes <wtb2@rice.edu>
2020-03-06 03:18:56 -0500

    test descaling function for varying kinds of inputs

--- fiasco/util/tests/test_tools.py
+++ fiasco/util/tests/test_tools.py
@@ -1,15 +1,45 @@
 import numpy as np
 
-from fiasco.util import burgess_tully_descale_vectorize, burgess_tully_descale
+from fiasco.util import burgess_tully_descale
 
 
-def test_burgess_tully():
-    # Check that normal and vectorized versions give the same result
+def test_burgess_tully_one_array():
     x = np.linspace(0, 1, 10)
     y = x**1.6 + 10
-    energy_ratio = np.ones(x.shape)
+    energy_ratio = np.ones((1,) + x.shape)
     c = 1
     for scaling_type in range(1, 7):
-        out = burgess_tully_descale(x, y, energy_ratio, c, scaling_type)
-        out_vect = burgess_tully_descale_vectorize(x, y, energy_ratio, c, scaling_type)
-        np.testing.assert_equal(out, out_vect[0])
+        ups = burgess_tully_descale(x, y, energy_ratio, c, scaling_type)
+        assert ups.shape == energy_ratio.shape
+
+
+def test_burgess_tully_2d_array():
+    x = np.tile(np.linspace(0, 1, 10), (2, 1))
+    y = x**1.6 + 10
+    energy_ratio = np.ones(x.shape[:1] + (10,))
+    c = 1*np.ones(x.shape[:1])
+    for scaling_type in range(1, 7):
+        st = scaling_type*np.ones(c.shape)
+        ups = burgess_tully_descale(x, y, energy_ratio, c, st)
+        assert ups.shape == energy_ratio.shape
+
+
+def test_burgess_tully_different_scaling_types():
+    x = np.tile(np.linspace(0, 1, 10), (6, 1))
+    y = x**1.6 + 10
+    energy_ratio = np.ones(x.shape[:1] + (10,))
+    c = 1*np.ones(x.shape[:1])
+    scaling_type = np.arange(1, 7)
+    ups = burgess_tully_descale(x, y, energy_ratio, c, scaling_type)
+    assert ups.shape == energy_ratio.shape
+
+
+def test_burgess_tully_staggered_array():
+    x = np.array([np.linspace(0, 1, 5), np.linspace(0, 1, 9)])
+    y = x**1.6 + 10
+    energy_ratio = np.ones(x.shape[:1] + (10,))
+    c = 1*np.ones(x.shape)
+    for scaling_type in range(1, 7):
+        st = scaling_type*np.ones(c.shape)
+        ups = burgess_tully_descale(x, y, energy_ratio, c, st)
+        assert ups.shape == energy_ratio.shape
