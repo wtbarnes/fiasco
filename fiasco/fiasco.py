fix bug in p-e ratio calculation for missing abundances

fiasco/fiasco.py
@@ -78,26 +78,36 @@ def proton_electron_ratio(temperature: u.K, **kwargs):
     .. [1] Young, P. et al., 2003, ApJS, `144 135 <http://adsabs.harvard.edu/abs/2003ApJS..144..135Y>`_
     """
     h_2 = fiasco.Ion('H +1', temperature, **kwargs)
-    numerator = h_2.abundance * h_2._ioneq[h_2._dset_names['ioneq_filename']]['ionization_fraction']
+    numerator = h_2.abundance * h_2._ioneq[h_2._instance_kwargs['ioneq_filename']]['ionization_fraction']
     denominator = u.Quantity(np.zeros(numerator.shape))
     for el_name in list_elements(h_2.hdf5_dbase_root):
-        el = fiasco.Element(el_name, temperature, **kwargs)
-        abundance = el.abundance
-        if abundance is None:
-            warnings.warn(f'Not including {el.atomic_symbol}. Abundance not available.')
+        el = fiasco.Element(el_name, temperature, **h_2._instance_kwargs)
+        try:
+            abundance = el.abundance
+        except KeyError:
+            abund_file = el[0]._instance_kwargs['abundance_filename']
+            warnings.warn(
+                f'Not including {el.atomic_symbol}. Abundance not available from {abund_file}.')
             continue
         for ion in el:
-            ioneq = ion._ioneq[ion._dset_names['ioneq_filename']]['ionization_fraction']
-            if ioneq is None:
-                warnings.warn(f'Not including {ion.ion_name}. Ionization fraction not available.')
+            ioneq_file = ion._instance_kwargs['ioneq_filename']
+            # NOTE: We use ._ioneq here rather than .ioneq to avoid doing an interpolation to the 
+            # temperature array every single time and instead only interpolate once at the end.
+            # It is assumed that the ioneq temperature array for each ion is the same.
+            try:
+                ioneq = ion._ioneq[ioneq_file]['ionization_fraction']
+                t_ioneq = ion._ioneq[ioneq_file]['temperature']
+            except KeyError:
+                warnings.warn(
+                    f'Not including {ion.ion_name}. Ionization fraction not available from {ioneq_file}.')
                 continue
             denominator += ioneq * abundance * ion.charge_state
 
     ratio = numerator / denominator
-    interp = interp1d(ion._ioneq[ion._dset_names['ioneq_filename']]['temperature'].value,
-                      ratio.value,
-                      kind='linear',
-                      bounds_error=False,
-                      fill_value=(ratio[0], ratio[-1]))
+    f_interp = interp1d(t_ioneq.to(temperature.unit).value,
+                        ratio.value,
+                        kind='linear',
+                        bounds_error=False,
+                        fill_value=(ratio[0], ratio[-1]))
 
-    return u.Quantity(interp(temperature))
+    return u.Quantity(f_interp(temperature.value))