060eff7b79cd0fa678bd8ba9bf14a2676601a8a2
Nick Murphy <namurphy@cfa.harvard.edu>
2022-11-21 15:40:41 -0500

    Migrate os.path -> pathlib in io/non_ion_sources.py

--- fiasco/io/sources/non_ion_sources.py
+++ fiasco/io/sources/non_ion_sources.py
@@ -39,7 +39,7 @@ class AbundParser(GenericParser):
         return df
 
     def to_hdf5(self, hf, df):
-        dataset_name = os.path.splitext(os.path.basename(self.filename))[0]
+        dataset_name = self.filename.stem
         footer = f"""{dataset_name}
 ------------------
 {df.meta['footer']}"""
@@ -67,8 +67,7 @@ class IoneqParser(GenericParser):
 
     def __init__(self, ioneq_filename, **kwargs):
         super().__init__(ioneq_filename, **kwargs)
-        self.full_path = kwargs.get('full_path',
-                                    os.path.join(self.ascii_dbase_root, 'ioneq', self.filename))
+        self.full_path = kwargs.get('full_path', self.ascii_dbase_root / 'ioneq' / self.filename)
 
     def preprocessor(self, table, line, index):
         if index == 0:
@@ -83,7 +82,7 @@ class IoneqParser(GenericParser):
             table.append(line)
 
     def to_hdf5(self, hf, df):
-        dataset_name = os.path.splitext(os.path.basename(self.filename))[0]
+        dataset_name = self.filename.stem
         for row in df:
             el = plasmapy.particles.atomic_symbol(int(row['Z'])).lower()
             ion = int(row['ion'])
@@ -113,11 +112,10 @@ class IpParser(GenericParser):
 
     def __init__(self, ip_filename, **kwargs):
         super().__init__(ip_filename, **kwargs)
-        self.full_path = kwargs.get('full_path',
-                                    os.path.join(self.ascii_dbase_root, 'ip', self.filename))
+        self.full_path = kwargs.get('full_path', self.ascii_dbase_root / 'ip' / self.filename)
 
     def to_hdf5(self, hf, df):
-        dataset_name = os.path.splitext(os.path.basename(self.filename))[0]
+        dataset_name = filename.stem
         footer = f"""{dataset_name}
 ------------------
 {df.meta['footer']}"""
