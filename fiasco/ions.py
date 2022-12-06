fix bugs in free-bound calculation

fiasco/ions.py
@@ -138,6 +138,17 @@ Using Datasets:
         }
         return kwargs
 
+    def next_ion(self):
+        """
+        Return an `~fiasco.Ion` instance with the next highest ionization stage.
+
+        For example, if the current instance is Fe XII (+11), this method returns
+        an instance of Fe XIII. All other input arguments remain the same.
+        """
+        return type(self)((self.atomic_number, self.ionization_stage+1),
+                           self.temperature,
+                           **self._instance_kwargs)
+
     @property
     @needs_dataset('elvlc', 'wgfa')
     def transitions(self):
@@ -1050,14 +1061,10 @@ Using Datasets:
         -----
         Does not include ionization equilibrium or abundance.
         """
-        # See also: _verner_cross_section
         wavelength = np.atleast_1d(wavelength)
         prefactor = (2/np.sqrt(2*np.pi)/(4*np.pi)/(
             const.h*(const.c**3) * (const.m_e * const.k_B)**(3/2)))
-        recombining = Ion(f'{self.element_name} {self.ionization_stage + 1}',
-                          self.temperature,
-                          hdf5_dbase_root=self.hdf5_dbase_root,
-                          **self._dset_names)
+        recombining = self.next_ion()
         try:
             # NOTE: This checks whether the fblvl data is available for the
             # recombining ion
@@ -1067,13 +1074,19 @@ Using Datasets:
             omega_0 = 1.0
         E_photon = const.h * const.c / wavelength
         energy_temperature_factor = np.outer(self.temperature**(-3/2), E_photon**5)
+        # Fill in observed energies with theoretical energies
+        E_obs = self._fblvl['E_obs']*const.h*const.c
+        E_th = self._fblvl['E_th']*const.h*const.c
+        level_fb = self._fblvl['level']
+        use_theoretical = np.logical_and(E_obs==0*u.erg, level_fb!=1)
+        E_fb = np.where(use_theoretical, E_th, E_obs)
         # Sum over levels of recombined ion
         sum_factor = u.Quantity(np.zeros(self.temperature.shape + wavelength.shape), 'cm^2')
         for omega, E, n, L, level in zip(self._fblvl['multiplicity'],
-                                         self._fblvl['E_obs']*const.h*const.c,
+                                         E_fb,
                                          self._fblvl['n'],
                                          self._fblvl['L'],
-                                         self._fblvl['level']):
+                                         level_fb):
             # Energy required to ionize ion from level i
             E_ionize = self.ip - E
             # Check if ionization potential and photon energy sufficient