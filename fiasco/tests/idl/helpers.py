174b07071291873e494f82b8fba855c6c7950de8
Will Barnes <will.t.barnes@gmail.com>
2024-01-17 18:19:20 -0500

    added a function for retrieving idl test data

--- fiasco/tests/idl/helpers.py
+++ fiasco/tests/idl/helpers.py
@@ -7,7 +7,7 @@ from astropy.utils.data import get_pkg_data_path
 
 from fiasco.util import read_chianti_version
 
-__all__ = ['save_idl_test_output']
+__all__ = ['save_idl_test_output', 'get_idl_test_output']
 
 
 def save_idl_test_output(variables, name, ascii_dbase_root):
@@ -18,3 +18,15 @@ def save_idl_test_output(variables, name, ascii_dbase_root):
     file_path =  data_dir / f"{name}_v{version}.asdf"
     with asdf.AsdfFile(variables) as af:
         af.write_to(file_path)
+
+
+def get_idl_test_output(name, version, keys=None):
+    # NOTE: Importing here to avoid it as a hard dependency for running tests
+    import asdf
+    data_dir = pathlib.Path(get_pkg_data_path('data', package='fiasco.tests.idl'))
+    file_path = data_dir / f"{name}_v{version}.asdf"
+    with asdf.open(file_path, copy_arrays=True) as af:
+        if keys is None:
+            keys = af.tree.keys()
+        output = {k: af.tree[k] for k in keys}
+    return output
