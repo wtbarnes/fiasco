ae5859b6d509f769cc71e85cd232049f14bd321d
Nabil Freij <nabil.freij@gmail.com>
2024-05-22 11:24:30 -0600

    reduce top level name space

--- fiasco/io/__init__.py
+++ fiasco/io/__init__.py
@@ -3,7 +3,7 @@ Parsers for raw CHIANTI atomic data.
 fiasco uses these parsers to transform the raw ASCII files into an HDF5 version of the CHIANTI database.
 """
 from fiasco.io.datalayer import DataIndexer, DataIndexerHDF5
-from fiasco.io.factory import Parser, ParserFactory
+from fiasco.io.factory import Parser
 from fiasco.io.generic import GenericIonParser, GenericParser
 
-__all__ = ["DataIndexer", "DataIndexerHDF5", "Parser", "ParserFactory", "GenericIonParser", "GenericParser"]
+__all__ = ["DataIndexer", "DataIndexerHDF5", "Parser", "GenericIonParser", "GenericParser"]
