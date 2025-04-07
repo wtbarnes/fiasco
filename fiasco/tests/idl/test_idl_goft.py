52823df29775fa7428821f2e1ade03e77d0ae20e
Will Barnes <will.t.barnes@gmail.com>
2025-04-07 17:10:50 -0400

    narrow region of comparison for goft results

--- fiasco/tests/idl/test_idl_goft.py
+++ fiasco/tests/idl/test_idl_goft.py
@@ -119,6 +119,6 @@ def test_idl_compare_goft(idl_env, hdf5_dbase_root, dbase_version, chianti_idl_v
     # where the contribution function is above some threshold relative to the peak of the
     # contribution function.
     goft_idl = idl_result['contribution_function']
-    i_compare = np.where(goft_idl>=goft_idl.max()*1e-6)
+    i_compare = np.where(goft_idl>=goft_idl.max()*1e-3)
     # Compare results
     assert u.allclose(goft_idl[i_compare], goft_python[i_compare], atol=None, rtol=0.05)
