f57a5e36e6ab359f740118cb78a6fae973a4a0e5
Will Barnes <will.t.barnes@gmail.com>
2025-07-25 13:14:40 -0400

    supply default values when calling getoption

--- fiasco/tests/idl/conftest.py
+++ fiasco/tests/idl/conftest.py
@@ -9,8 +9,8 @@ def idl_env(ascii_dbase_root, request):
     # is that the IDL tests can still be run if there is a file containing the IDL
     # results available. Thus, we have to wait until later to decide if we want to
     # skip the test.
-    idl_executable = request.config.getoption('--idl-executable')
-    idl_codebase_root = request.config.getoption('--idl-codebase-root')
+    idl_executable = request.config.getoption('--idl-executable', default=None)
+    idl_codebase_root = request.config.getoption('--idl-codebase-root', default=None)
     if idl_executable is None or idl_codebase_root is None:
         return None
     return setup_idl_environment(ascii_dbase_root, idl_codebase_root, idl_executable)
@@ -18,7 +18,7 @@ def idl_env(ascii_dbase_root, request):
 
 @pytest.fixture(scope="session")
 def chianti_idl_version(request):
-    idl_codebase_root = request.config.getoption('--idl-codebase-root')
+    idl_codebase_root = request.config.getoption('--idl-codebase-root', default=None)
     if idl_codebase_root is None:
         return None
     return get_chianti_idl_version(idl_codebase_root)
