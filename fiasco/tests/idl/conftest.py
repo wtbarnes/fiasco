ce0cd45644cf89dc043b79881cdb7083e74c2230
Will Barnes <will.t.barnes@gmail.com>
2025-07-25 16:42:58 -0400

    Supply default value for custom pytest commandline arguments when calling `getoption` (#389)
    * put everything in one conftest
    
    * Revert "put everything in one conftest"
    
    This reverts commit b002c793bf015be54274f48a9763d760996a1365.
    
    * supply default values when calling getoption
    
    (cherry picked from commit b85c46681cd2086f2fd2343269b40b93c7efd5ff)

--- fiasco/tests/idl/conftest.py
+++ fiasco/tests/idl/conftest.py
@@ -9,8 +9,8 @@ def idl_env(ascii_dbase_root, request):
     # is that the IDL tests can still be run if there is a file containing the IDL
     # results available. Thus, we have to wait until later to decide if we want to
     # skip the test.
-    idl_executable = request.config.getoption('--idl-executable')
-    idl_codebase_root = request.config.getoption('--idl-codebase-root')
+    idl_executable = request.config.getoption('--idl-executable', default=None)
+    idl_codebase_root = request.config.getoption('--idl-codebase-root', default=None)
     if idl_executable is None or idl_codebase_root is None:
         return None
     return setup_idl_environment(ascii_dbase_root, idl_codebase_root, idl_executable)
