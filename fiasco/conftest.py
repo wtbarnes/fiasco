28ed29477048c95d40043ff88edd7571a572bc6d
Will Barnes <will.t.barnes@gmail.com>
2025-07-25 13:01:19 -0400

    Revert "put everything in one conftest"
    This reverts commit b002c793bf015be54274f48a9763d760996a1365.

--- fiasco/conftest.py
+++ fiasco/conftest.py
@@ -15,20 +15,6 @@ except ImportError:
 else:
     matplotlib.use('Agg')
 
-
-def pytest_addoption(parser):
-    parser.addoption('--ascii-dbase-root', action='store', default=None)
-    parser.addoption('--ascii-dbase-url', action='store', default=None)
-    parser.addoption('--hdf5-dbase-root', action='store', default=None)
-    parser.addoption('--disable-file-hash', action='store_true', default=False,
-                     help='Disable MD5 hash checks on test files')
-    parser.addoption('--idl-executable', action='store', default=None)
-    parser.addoption('--idl-codebase-root', action='store', default=None)
-    parser.addoption('--include-all-files', action='store_true', default=False)
-    parser.addoption('--skip-version-check', action='store_true', default=False,
-                     help='Do not check CHIANTI version')
-
-
 @pytest.fixture(scope='session')
 def ascii_dbase_tree(tmpdir_factory, request):
     path = request.config.getoption('--ascii-dbase-root')
