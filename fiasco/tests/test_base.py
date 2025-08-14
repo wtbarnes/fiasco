d3ea8c7f8d03d52815619aae52e9b2917e39ac51
Will Barnes <will.t.barnes@gmail.com>
2025-08-14 09:21:05 -0400

    Backport PR #409: Fix warning message formatting for out-of-date dbase

--- fiasco/tests/test_base.py
+++ fiasco/tests/test_base.py
@@ -1,10 +1,16 @@
 """
 Test base ion functionality
 """
+import h5py
+import logging
+import numpy as np
 import pytest
+
+from packaging.version import Version
 
 import fiasco
 
+from fiasco.util import build_hdf5_dbase
 from fiasco.util.exceptions import MissingIonError
 
 
@@ -45,3 +51,21 @@ def test_create_invalid_ion_raises_missing_ion_error(hdf5_dbase_root):
 def test_isoelectronic_sequence(ion_name, sequence_name, hdf5_dbase_root):
     ion = fiasco.base.IonBase(ion_name, hdf5_dbase_root=hdf5_dbase_root)
     assert ion.isoelectronic_sequence == sequence_name
+
+
+def test_old_fiasco_version_warning(tmpdir_factory, caplog):
+    path = tmpdir_factory.mktemp('fiasco').join('chianti_dbase_empty_old_version.h5')
+    build_hdf5_dbase(None, path, files=[], check_hash=False, overwrite=True)
+    test_ion = 'H 1'
+    with h5py.File(path, mode='a') as hf:
+        current_version = Version(hf.attrs['fiasco_version'])
+        old_version = Version(
+            f'{current_version.major}.{current_version.minor-1}.{current_version.micro}',
+        )
+        hf.attrs['fiasco_version'] = str(old_version)
+        del hf['ion_index']
+        ds = hf.create_dataset('ion_index', data=np.array([test_ion,]).astype(np.bytes_))
+        ds.attrs['unit'] = 'SKIP'
+    with caplog.at_level(logging.WARN):
+        fiasco.base.IonBase(test_ion, hdf5_dbase_root=path)
+    assert 'was produced with an earlier version of fiasco' in caplog.text
